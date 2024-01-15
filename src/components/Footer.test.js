48b7d89039237a8334902ee998bdda3289e20d7d
Francis Hamzagic <francishamzagic@gmail.com>
2024-01-15 11:13:47 -0300

    jest test commit

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -9,7 +9,7 @@ describe('<Footer />', () => {
         <Footer />
       </MemoryRouter>
     );
-    const footerText = screen.getByText(/2024 techfran/i);
-    expect(footerText).not.toBeInTheDocument();
+    const footerText = screen.getByText(/hello/i);
+    expect(footerText).toBeInTheDocument();
   });
 });
